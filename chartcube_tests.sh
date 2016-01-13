705db09de87083b6f75d582136ecf573877439ff
karuna lingham <karuna.lingham@codepandora.com>
2016-01-13 17:23:45 +0530

    Added Pass/Fail color display for test suite, count of total tests executed

--- chartcube_tests.sh
+++ chartcube_tests.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 
 d="$(date +'%d-%m-%Y')"
-t="$(date +%r)"
+t="$(date +%H:%M)"
 now=$d-$t
 
 #Begin console log
-exec > >(tee -i logs/chartcube_logs_$now.txt)
+exec > >(tee -i logs/chart_logs_$now.txt)
 #copy stderr to console output
 exec 2>&1
 
 echo '''
-Copyright (C) 2015  CODE PANDORA
+Copyright (C) 2015-2016  CODE PANDORA
 Created on 2015-12-11
+Updated on 2016-01-13
+
    __________  ____  ______   ____  ___    _   ______  ____  ____  ___
   / ____/ __ \/ __ \/ ____/  / __ \/   |  / | / / __ \/ __ \/ __ \/   |
  / /   / / / / / / / __/    / /_/ / /| | /  |/ / / / / / / / /_/ / /| |
@@ -23,25 +25,52 @@ Created on 2015-12-11
 
 echo "================================="
 echo "Chartcube App v1.4 ..."
-echo "Running Test Suite v1.0 ..."
+echo "Running Test Suite v1.13 ..."
 echo "================================="
+
+text1="OK"
+text2="ERROR"
+total_count=0
 
 #Begin iterating through every folder
 for folder in ./*
 do
+
   #Begin recording tests for every folder
   adb shell screenrecord /sdcard/$folder.mp4 &
   echo "------------------"
   echo "$folder"
   echo "------------------"
+
   #Begin iterating through every file in specified folder
   for fname in $folder/*.py
   do
+
+    #Passed tests
+    if grep --quiet $text1 logs/chart_logs_$now.txt; then
+      echo "Tests Passed: "
+      echo -e "\e[32m" #green
+      grep -2A1 $text1 "logs/chart_logs_$now.txt"
+      echo -e "\e[39m" #default color
+    fi
+
+    #Failed tests
+    if grep --quiet $text2 logs/chart_logs_$now.txt; then
+      echo "Tests Failed: "
+      echo -e "\e[31m" #red
+      grep -1A3 $text2 "logs/chart_logs_$now.txt"
+      echo -e "\e[39m" #default color
+    fi
+
     #Indicate when testing an Interim file
     if [ $(find $fname -regex .*interim.*\.py) ]
     then
       echo "*****$fname: INTERIM FILE*****"
     fi
+
+    #Execute file
     python $fname
+    total_count=$((total_count + 1))
   done
+  echo "Tests executed: $total_count"
 done
